beaa653ae7ca2d514bf68d2f984fbb31fa5f251a
daniel <arenhart.daniel@gmail.com>
2023-04-20 14:26:14 -0300

    format output as a table

--- script.js
+++ script.js
@@ -1,20 +1,55 @@
-document.getElementById('csvFile').addEventListener('change', function(event) {
+function parseCsv(results) {
+  let tallyCounts = {};
+
+  for (let i = 0; i < results.data.length; i++) {
+    let row = results.data[i];
+    let sku = row["Lineitem sku"];
+    let quantity = parseInt(row["Lineitem quantity"]);
+
+    if (tallyCounts.hasOwnProperty(sku)) {
+      tallyCounts[sku] += quantity;
+    } else {
+      tallyCounts[sku] = quantity;
+    }
+  }
+
+  let tableHtml = `
+        <table border="1">
+            <tr>
+                <th>SKU</th>
+                <th>Tally Count</th>
+            </tr>
+        `;
+
+  for (let sku in tallyCounts) {
+    tableHtml += `
+                <tr>
+                    <td>${sku}</td>
+                    <td>${tallyCounts[sku]}</td>
+                </tr>
+            `;
+  }
+
+  tableHtml += `</table>`;
+
+  let output = document.getElementById("output");
+  output.innerHTML = tableHtml;
+}
+
+document
+  .getElementById("inputfile")
+  .addEventListener("change", function (event) {
     let file = event.target.files[0];
     let reader = new FileReader();
 
-    reader.onload = function(progressEvent) {
-        let lines = this.result.split('\n');
-        let count = 0;
+    reader.onload = function (progressEvent) {
+      let csvContent = this.result;
 
-        for (let i = 0; i < lines.length; i++) {
-            let columns = lines[i].split(',');
-            if (columns[0].includes("foo")) {
-                count++;
-            }
-        }
-        
-        document.getElementById('output').innerText = 'Number of rows containing "foo" in the first column: ' + count;
+      Papa.parse(csvContent, {
+        header: true,
+        complete: parseCsv,
+      });
     };
-    
+
     reader.readAsText(file);
-});
+  });
